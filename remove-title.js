515eb5bb6ec12d2f9a54a06aa9b74a33600c6344
Andreas Egli <github@kharan.ch>
2022-05-10 14:16:14 +0200

    Update remove-title.js

--- remove-title.js
+++ remove-title.js
@@ -1,13 +1,8 @@
-function applyRules(tabId) {
-  let targetTab = browser.tabs.get(tabId);
-  browser.tabs.executeScript(tabId, {
-    code: "document.title = ' ';",
-    runAt: "document_start"
-  });
-}
-
 browser.tabs.onUpdated.addListener(
-    (tabId) => {
-        applyRules(tabId);
-    }
+  (tabId) => {
+    browser.tabs.executeScript(tabId, {
+      code: "document.title = ' ';",
+      runAt: "document_start"
+    });
+  }
 );
